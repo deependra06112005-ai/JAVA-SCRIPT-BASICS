a3ae6b0e2ecf811bae4a30e192212a3e90e74ded
deependra06112005-ai <deependra06112005@gmail.com>
2026-02-22 13:41:40 +0000

    d2

--- prac.js
+++ prac.js
@@ -20,3 +20,17 @@ function fetchComments() {
         },1200);
     });
 } 
+async function async function loadDashboardSequential() {
+      console.time("Sequential");
+
+        const user = await fetchUser();
+          const posts = await fetchPosts();
+            const comments = await fetchComments();
+
+              console.log({ user, posts, comments });
+
+                console.timeEnd("Sequential");
+                }
+
+                loadDashboardSequential();
+}
