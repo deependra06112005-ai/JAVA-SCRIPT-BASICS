07e1ea18343a3b8f5be2f156de46ebc1f70650d7
deependra06112005-ai <deependra06112005@gmail.com>
2026-03-08 11:53:17 +0000

    Making The form using usestate on Change..

--- paintshop-frontend/src/App.js
+++ paintshop-frontend/src/App.js
@@ -2,19 +2,36 @@ import { useState } from  'react';
     
 function App(){
   const [showLowstock, setShowLowStock] = useState(false);
-  const paints = [
+  const[newPaint, setNewPaint] = useState({
+    name: "", price: 0, quantity: 0});
+  const [paints, setPaints ] = useState([
     { name: "Royal Shyne 10L", price: 1500, quantity: 10 }, 
     { name: "Royal Shyne 20L", price: 2800, quantity: 8 },
     {name: "Royal Luxuxry Shyne 20L", price: 3550, quantity:5},
     {name:"Royale Luxury Emulsion 10L", price: 1500, quantity: 10},
     {name:"Royale Luxury Emulsion 20L", price: 2800, quantity: 8},
     {name:"Royale Luxury Emulsion 5L", price: 800, quantity: 15},
-  ]
+  ]);
   const dislayPaints = showLowstock ? paints.filter(paint => paint.quantity < 5) : paints;
   return(
     <div>
       <h1>Manu Hardware</h1>
       <h2>Paints</h2>
+      <div>
+        <input type ="Paint Name" placeholder ="Paint Name" value={newPaint.name}
+         onChange={(e) => setNewPaint({...newPaint, name: e.target.value})}/>
+         <input type ="Price" placeholder ="Price" value={newPaint.price}
+          onChange={(e) => setNewPaint({...newPaint, price: e.target.value})}/>
+         <input type ="Quantity" placeholder ="Quantity" value={newPaint.quantity}
+          onChange={(e) => setNewPaint({...newPaint, quantity: e.target.value})}/>  
+          <button onClick={() => {
+            setPaints([...paints, newPaint]);
+            setNewPaint({ name: "", price: 0, quantity: 0 });
+          }}>
+            Add Paint
+          </button>
+
+      </div>
       <button onClick={()=> setShowLowStock(!showLowstock)}>
         {showLowstock ? "Show All Paints" : "Show Low Stock"} 
       </button>
