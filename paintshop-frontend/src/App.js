d5a6e40213ce3f25fdfe6dd2cace936e3a4ae934
deependra06112005-ai <deependra06112005@gmail.com>
2026-03-08 11:22:21 +0000

    User State concept

--- paintshop-frontend/src/App.js
+++ paintshop-frontend/src/App.js
@@ -1,17 +1,24 @@
+import { useState } from  'react';  
+    
 function App(){
-  
-  const paints =[
+  const [showLowstock, setShowLowStock] = useState(false);
+  const paints = [
+    { name: "Royal Shyne 10L", price: 1500, quantity: 10 }, 
     { name: "Royal Shyne 20L", price: 2800, quantity: 8 },
     {name: "Royal Luxuxry Shyne 20L", price: 3550, quantity:5},
     {name:"Royale Luxury Emulsion 10L", price: 1500, quantity: 10},
     {name:"Royale Luxury Emulsion 20L", price: 2800, quantity: 8},
     {name:"Royale Luxury Emulsion 5L", price: 800, quantity: 15},
   ]
+  const dislayPaints = showLowstock ? paints.filter(paint => paint.quantity < 5) : paints;
   return(
     <div>
       <h1>Manu Hardware</h1>
       <h2>Paints</h2>
-      {paints.map(paint => (
+      <button onClick={()=> setShowLowStock(!showLowstock)}>
+        {showLowstock ? "Show All Paints" : "Show Low Stock"} 
+      </button>
+      {dislayPaints.map(paint => (
         <div key ={paint.name}>
           <h3>{paint.name}</h3>
           <p>price: {paint.price}</p>
