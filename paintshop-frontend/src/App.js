4fcd5d95923f5ef68d3f98154d707713dd6d3c29
deependra06112005-ai <deependra06112005@gmail.com>
2026-03-09 12:31:12 +0000

    Edit Quantity button

--- paintshop-frontend/src/App.js
+++ paintshop-frontend/src/App.js
@@ -13,6 +13,18 @@ function App(){
     {name:"Royale Luxury Emulsion 10L", price: 1500, quantity: 10},
     {name:"Royale Luxury Emulsion 20L", price: 2800, quantity: 8},
     {name:"Royale Luxury Emulsion 5L", price: 800, quantity: 15},
+    {name:"Apcolite Premium Gloss Enamel 1L", price: 400, quantity: 20},
+    {name:"Apcolite Premium Gloss Enamel 4L", price: 1200, quantity: 3},
+    {name:"Apcolite Premium Gloss Enamel 10L", price: 2800, quantity: 7},
+    {name:"Apcolite Premium Gloss Enamel 20L", price: 5000, quantity: 2},
+    {name:"Tractor Sparc 1L", price: 350, quantity: 25},
+    {name:"Tractor Sparc 4L", price: 1000, quantity: 10},
+    {name:"Tractor Sparc 10L", price: 2500, quantity: 5},
+    {name:"Tractor Sparc 20L", price: 4500, quantity: 1},
+    {name:"Dulux Weathershield 1L", price: 500, quantity: 30},
+    {name:"Dulux Weathershield 4L", price: 1500, quantity: 12},
+    {name:"Dulux Weathershield 10L", price: 3500, quantity: 6},
+    {name:"Dulux Weathershield 20L", price: 6000, quantity: 3},
   ]);
   const dislayPaints = showLowstock ? paints.filter(paint => paint.quantity < 5) : paints;
   return(
@@ -58,7 +70,26 @@ function App(){
           <h3>{paint.name}</h3>
           <button onClick ={()=> setEditingPaint(paint.name)}>Edit Quantity</button>
           <p>price: {paint.price}</p>
-          <p>Quantity: {paint.quantity}</p>
+          {editingPaint === paint.name ? (
+            <div>
+              <input 
+              value ={newQuantity}
+              onChange={(e) => setNewQuantity(e.target.value)}
+              placeholder="New Quantity"
+              />
+              <button onClick={() => {
+                setPaints(paints.map(p => p.name === editingPaint ? {...p, quantity: newQuantity }:p))
+                setEditingPaint(null);
+                setNewQuantity("");
+              }}>
+                Save
+              </button>
+            </div>
+          ) : (
+            <p>quantity: {paint.quantity}</p>
+          )}
+
+
           <p>{paint.quantity <5 ? "red LOW Stock" : " green In Stock"}</p>
         </div>
       ))}
